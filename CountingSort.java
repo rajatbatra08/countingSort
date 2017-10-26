d502bc97a42e7c7e7b00970032cae85cee4ccaeb
rajatbatra08 <33134840+rajatbatra08@users.noreply.github.com>
2017-10-27 01:09:16 +0530

    Add files via upload

---
+++ CountingSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+class CountingSort
+{
+void sort(int[] arr)
+{
+int count[] =new int[10];
+int output[] = new int[arr.length];
+
+for(int i=0;i<arr.length;i++)
+{
+count[arr[i]%10]++;
+}
+for(int j=1;j<10;j++)
+{
+count[j]=count[j-1]+count[j];
+}
+for(int i=0;i<arr.length;i++)
+{
+output[count[arr[i]%10]-1]=arr[i];
+count[arr[i]%10]--;
+}
+for(int i=0;i<arr.length;i++)
+{
+System.out.print(output[i]+" ");
+}
+}
+
+
+public static void main(String[] args)
+{
+int[] arr={10,13,13,11,12,10,19};                    
+CountingSort ss = new CountingSort();
+ss.sort(arr);
+
+}
+}
